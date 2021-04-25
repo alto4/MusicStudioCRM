07d6f38d951dd2c76413f17ea21edd69fa21ece6
Scott Alton <scottaltonmusic@gmail.com>
2021-04-25 16:29:29 -0400

    Updated database connection credentials for Heroku PostgreSQL deployment.

--- includes/constants.php
+++ includes/constants.php
@@ -14,12 +14,19 @@ define("CLIENT", "c");
 define("PENDING", "p");
 define("DISABLED", "d");
 
-// Database Constants
-define("DB_HOST", "localhost");
-define("DATABASE", "altons_db");
-define("DB_ADMIN", "altons");
+// Local Database Constants
+// define("DB_HOST", "localhost");
+// define("DATABASE", "altons_db");
+// define("DB_ADMIN", "altons");
+// define("DB_PORT", "5432");
+// define("DB_PASSWORD", "password");
+
+// Heroku Deployment Database Constants
+define("DB_HOST", "ec2-52-23-190-126.compute-1.amazonaws.com");
+define("DATABASE", "dadp580pstblkp");
+define("DB_ADMIN", "kxvexocjxvweao");
 define("DB_PORT", "5432");
-define("DB_PASSWORD", "password");
+define("DB_PASSWORD", "981d6c971aec0d6649be8a46f7a40c6f9fdc84ab29afd2a19e97cd789c23a0e2");
 
 // Registration Validation Constants
 define("MIN_PASSWORD_LENGTH", 3);
